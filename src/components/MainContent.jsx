57777e5eb34bfa5971353f9f43a6e46466b15693
dimpinis9 <dimpinis9@gmail.com>
2024-01-29 00:34:32 +0200

    Cursive

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../assets/MainContent.css';
 import image1 from '../assets/images/image1.jpg';
 import image2 from '../assets/images/image2.jpg';
@@ -6,11 +6,12 @@ import image3 from '../assets/images/image3.jpg';
 import { useNavigate } from 'react-router-dom';
 
 export const MainContent = () => {
+
   useEffect(() => {
     const slides = document.querySelectorAll('.mySlides');
     let currentSlide = 0;
 
-    slides[currentSlide].classList.add('showing'); 
+    slides[currentSlide].classList.add('showing');
 
     const nextSlide = () => {
       slides[currentSlide].classList.remove('showing');
@@ -20,13 +21,18 @@ export const MainContent = () => {
 
     const slideInterval = setInterval(nextSlide, 3000);
 
+   
+   
     return () => clearInterval(slideInterval);
   }, []);
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   return (
     <main>
+      <div className="product-title">
+      <h1 className="typing-text">LOOK AMAZING AND FEEL AS FAB AS EVER</h1>
+      </div>
       <div className="content-container">
         <div className="slideshow-container">
           <div className="mySlides fade">
@@ -42,15 +48,26 @@ export const MainContent = () => {
       </div>
       <section className="hero">
         <div className="product-description">
-          <h1>LOOK AMAZING AND FEEL AS FAB AS EVER</h1>
+          <h1>Castine</h1>
+          <h2>Natural Dry Oil</h2>
           <p>Φυσικό λάδι για τα μαλλιά και το δέρμα - το μυστικό ομορφιάς σας.</p>
-          {/* Χρησιμοποιήστε το onClick για την πλοήγηση προς τη σελίδα /products */}
           <button onClick={() => navigate('/products')}>Ανακαλύψτε περισσότερα</button>
         </div>
       </section>
+      <div className="product-summary">
+        <h2>Συντόμη Περιγραφή Προϊόντος</h2>
+        <p className="summary-text">
+          Φυσικό λάδι για τα μαλλιά και το δέρμα - το μυστικό ομορφιάς σας.
+          Το προϊόν μας περιέχει μια μοναδική φόρμουλα που ενυδατώνει τα μαλλιά
+          σας και προσφέρει απαλότητα στο δέρμα. Ανακαλύψτε τα οφέλη τώρα!
+        </p>
+        <button onClick={() => navigate('/products')}>Παραγγελία</button>
+      </div>
       <section className="additional-content">
         {/* Προσθέστε εδώ περισσότερο περιεχόμενο και φωτογραφίες */}
       </section>
     </main>
   );
 };
+
+
