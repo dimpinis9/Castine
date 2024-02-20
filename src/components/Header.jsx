7c062f1f3e62b86646608e530ee69089b7e29b88
dimpinis9 <dimpinis9@gmail.com>
2024-02-20 23:26:20 +0200

    Header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../assets/Header.css';
 import CartModal from './CartModal'; 
+import logo from '../assets/images/castine_logo.png'
 
 export const Header = () => {
   const [isCartModalOpen, setCartModalOpen] = useState(false);
@@ -17,14 +18,16 @@ export const Header = () => {
   return (
     <header>
       <div className="logo">
-         <h1>CASTINE</h1>
+      <img src={logo} alt="" />
       </div>
       <nav className={isMenuOpen ? "open" : ""}>
         <ul>
+        
           <li><a href="/">ΑΡΧΙΚΗ</a></li>
           <li><a href="/products">ΠΡΟΙΟΝ</a></li>
           <li><a href="/about">ABOUT US</a></li>
           <li><a href="/contact">ΕΠΙΚΟΙΝΩΝΙΑ</a></li>
+          <li><button onClick={() => setMenuOpen(false)}><i className="fas fa-times"></i></button></li>
         </ul>
       </nav>
       <div className="menu-icon" onClick={() => setMenuOpen(!isMenuOpen)}>
