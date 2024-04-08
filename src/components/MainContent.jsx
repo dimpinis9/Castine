83a58e167ca4fcd6790a9a08a627576d772eca86
dimpinis9 <dimpinis9@gmail.com>
2024-04-09 00:14:26 +0300

    Photo4

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../assets/MainContent.css';
 import { useSprings, animated } from 'react-spring';
-import productImage1 from '../assets/images/image1.jpg';
-import productImage2 from '../assets/images/image2.jpg';
-import productImage3 from '../assets/images/image3.jpg';
 import productImage4 from '../assets/images/image4.jpg';
 
 const AnimatedText = ({ text }) => {
@@ -29,41 +26,37 @@ const AnimatedText = ({ text }) => {
   );
 };
 
-const imageUrls = [productImage1, productImage2, productImage3, productImage4];
 
-export const MainContent = () => {
+export const  MainContent = () => {
   const [showHero, setShowHero] = useState(true);
-  const [currentImage, setCurrentImage] = useState(0);
+  const [opacity, setOpacity] = useState(0);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      console.log(showHero);
-      setShowHero(true);
-    }, 2000);
+    setShowHero(true);
 
-    const imageInterval = setInterval(() => {
-      setCurrentImage((currentImage) => (currentImage + 1) % imageUrls.length);
-    }, 5000); // Αλλαγή εικόνας κάθε 5 δευτερόλεπτα
+    let opacityTimeout = setTimeout(() => setOpacity(1), 2000); // Αλλάξτε σταδιακά τη διαφάνεια σε 1 σε 2 δευτερόλεπτα
 
-    return () => {
-      clearTimeout(timeout);
-      clearInterval(imageInterval);
-    };
+    return () => clearTimeout(opacityTimeout);
   }, []);
 
-  // Αντικατάσταση του video element με ένα div που περιέχει την ενεργή εικόνα
+  const imageUrls = [productImage4]; // Διατηρήστε μόνο την image4
+
   return (
     <main>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight:'5%', height: '90vh', marginTop:'-100px' }}>
-          <div className="image-container" style={{ position: 'relative', width: '100%', overflow: 'hidden', maxWidth: '100%', maxHeight: '100%' }}>
-            {showHero && (
-              <img src={imageUrls[currentImage]} style={{ width: "100%", height: "100%", objectFit: "contain" }} alt="Slideshow" />
-            )}
-          <div style={{ position: 'absolute', top: '50%', left: '40%', transform: 'translate(-50%, -50%)', width: '100%', opacity: 0.7, transition: 'opacity 1s ease-in-out' }}>
-        
-          </div>
-      
+     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100%', marginTop: '-100px' }}>
+     <div className="image-container">
+  {showHero && (
+    <img src={imageUrls[0]} style={{ width: "100%", height: "auto", opacity: opacity }} alt="Slideshow" />
+  )}
 </div>
+</div>
+      <div className="product-title">
+        <h1 className="deserve">
+          <AnimatedText text="YOU DESERVE IT"/>
+        </h1>
+        <h2 className="stay"><AnimatedText text="STAY BEAUTIFUL"/></h2>
+        <h2 className="inside"><AnimatedText text="INSIDE AND OUTSIDE" /></h2>
+    
       </div>
       <div className="oils-introduction-card">
   <h2>Τα φυσικά έλαια: Ελιξίρια ευεξίας και ομορφιάς</h2>
@@ -79,16 +72,7 @@ export const MainContent = () => {
   <p>Είναι ένα σύμπλεγμα φυσικών ελαίων άμεσης απορρόφησης που προσφέρει ακαταμάχητη λάμψη και άρωμα. Τα ελαια και τα λιπαρά που παράγει η επιδερμίδα συμβάλουν στην προστασία και την εξισορρόπηση του προστατευτικού φραγμού της αλλά συχνά, λόγω καιρικών συνθηκών, λάθος πρακτικών περιποίησης, το πέρασμα των ετών, η παραγωγή τους μειώνεται και τότε το δέρμα αρχίζει να αφυδατώνεται.</p>
   <p>Εφαρμόστε μετά το ντουζ σε νωπή επιδερμίδα για καλύτερα αποτελέσματα.</p>
 </div>
-      <div className="product-title">
-        <h1 className="deserve">
-          
-          <AnimatedText text="YOU DESERVE IT"/>
-        </h1>
-        <h2 className="stay"><AnimatedText text="STAY BEAUTIFUL"/></h2>
-        <h2 className="inside"><AnimatedText text="INSIDE AND OUTSIDE" /> </h2>
 
-        
-      </div>
       <div className="oils-introduction-card">
       <h2>CASTINE Dry Oil: Λάμψη & Ενυδάτωση</h2>
   <p>Είναι ένα σύμπλεγμα φυσικών ελαίων άμεσης απορρόφησης που προσφέρει ακαταμάχητη λάμψη και άρωμα.
