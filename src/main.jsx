018f367c89b151cae355e94ab18df81e12dedd4b
Dimitris <dimpinis9@gmail.com>
2024-05-27 23:50:12 +0300

    dist

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CartProvider } from "./CartContext";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';  
-import { CartProvider } from './CartContext';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './assets/styles.css';
-import './index.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root')); 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     <CartProvider>
+    <CartProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      </CartProvider>
+    </CartProvider>
   </React.StrictMode>
 );
